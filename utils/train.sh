5e03cb9482cbbba3548ed0833cd64d834b0ce98f
ryan nguyen <86136605+ryannguyen10@users.noreply.github.com>
2023-02-02 09:40:15 -0600

    Update train.sh

--- utils/train.sh
+++ utils/train.sh
@@ -4,6 +4,6 @@ cd data/
 mkdir -p mnist_rotation
 cd mnist_rotation
 
-wget https://www.dropbox.com/s/mdqkq3lps4uw59g/train_list_fairface.txt?dl=0 -O train_list_fairface.txt
+wget https://www.dropbox.com/s/g8q9aamjdu4fvt0/train_list_fairface.txt?dl=0 -O train_list_fairface.txt
 tar -zxf train_list_fairface.txt
 rm train_list_fairface.txt
