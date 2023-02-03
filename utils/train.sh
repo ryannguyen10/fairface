8afeb48fd889c0bd5d292559a7f193e61591db29
ryan nguyen <86136605+ryannguyen10@users.noreply.github.com>
2023-02-03 10:21:09 -0600

    Update train.sh

--- utils/train.sh
+++ utils/train.sh
@@ -1,9 +1,23 @@
+echo 'Building directory structure...'
 cd ../
 mkdir -p data/
 cd data/
 mkdir -p facial_image
 cd facial_image
 
-wget https://www.dropbox.com/s/g8q9aamjdu4fvt0/train_list_fairface.txt?dl=0 -O train_list_fairface.txt
-mv train_list_fairface.txt/* ./ 
-rm train_list_fairface.txt
+echo 'Directories done. Downloading data...'
+wget https://www.dropbox.com/s/ggq15lzghbeqxr3/fairface-img-margin025-trainval.zip?dl=0
+mv 5793795 fairface-img-margin025-trainval.zip
+
+echo 'Downloaded, unzipping files...'
+unzip fairface-img-margin025-trainval.zip
+
+echo 'moving files to correct directories...'
+mv train/* ./ 
+
+echo 'Removing unwanted files...'
+
+rm -r train
+
+echo 'All done!'
+
