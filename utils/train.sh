480e0e943a0ce4dd52cc01543d547b0ca8ca676c
ryan nguyen <86136605+ryannguyen10@users.noreply.github.com>
2023-02-03 13:48:52 -0600

    Update train.sh

--- utils/train.sh
+++ utils/train.sh
@@ -4,7 +4,7 @@ cd data/
 mkdir -p facial_image
 cd facial_image
 
-wget https://www.dropbox.com/s/gafl7acfi1xlqni/fairface-img-margin025-trainval.zip?dl=0
+wget https://www.dropbox.com/s/gafl7acfi1xlqni/fairface-img-margin025-trainval.zip?dl=0 -O fairface-img-margin025-trainval.zip
 unzip fairface-img-margin025-trainval.zip
 mv train fairface_train_data
 
