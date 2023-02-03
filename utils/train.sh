6ba5a6e5fac313d7876c2071fc919d8eaa357cb0
ryan nguyen <86136605+ryannguyen10@users.noreply.github.com>
2023-02-03 10:36:01 -0600

    Update train.sh

--- utils/train.sh
+++ utils/train.sh
@@ -1,23 +1,9 @@
-echo 'Building directory structure...'
 cd ../
 mkdir -p data/
 cd data/
 mkdir -p facial_image
 cd facial_image
 
-echo 'Directories done. Downloading data...'
-wget https://www.dropbox.com/s/ggq15lzghbeqxr3/fairface-img-margin025-trainval.zip?dl=0
-mv 5793795 fairface-img-margin025-trainval.zip
-
-echo 'Downloaded, unzipping files...'
-unzip fairface-img-margin025-trainval.zip
-
-echo 'moving files to correct directories...'
-mv train/* ./ 
-
-echo 'Removing unwanted files...'
-
-rm -r train
-
-echo 'All done!'
-
+wget https://www.dropbox.com/s/wdws3b3fjo190sk/self_generated.tar.gz?dl=0 -O self_generated.tar.gz
+tar -zxf self_generated.tar.gz
+rm self_generated.tar.gz
