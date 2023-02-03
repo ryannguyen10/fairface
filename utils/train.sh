b75e7ce73e4565638c1efcaf56813d7fb4304354
ryan nguyen <86136605+ryannguyen10@users.noreply.github.com>
2023-02-03 00:57:55 -0600

    Update train.sh

--- utils/train.sh
+++ utils/train.sh
@@ -1,8 +1,8 @@
 cd ../
 mkdir -p data/
 cd data/
-mkdir -p mnist_rotation
-cd mnist_rotation
+mkdir -p facial_image
+cd facial_image
 
 wget https://www.dropbox.com/s/g8q9aamjdu4fvt0/train_list_fairface.txt?dl=0 -O train_list_fairface.txt
 tar -zxf train_list_fairface.txt
