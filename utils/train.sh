444f83b7f2b98d0f4da7520c3bf36fc690e8f99c
ryan nguyen <86136605+ryannguyen10@users.noreply.github.com>
2023-03-24 09:50:58 -0500

    Update train.sh

--- utils/train.sh
+++ utils/train.sh
@@ -4,6 +4,6 @@ cd data/
 mkdir -p facial_image
 cd facial_image
 
-wget https://www.dropbox.com/s/3nuv437s8z9x5wm/dataset.tar.gz?dl=0 -O dataset.tar.gz
+wget https://www.dropbox.com/s/0601itdnoecm8x7/ezyZip.tar.gz?dl=0 -O dataset.tar.gz
 tar -zxf dataset.tar.gz
 rm dataset.tar.gz
